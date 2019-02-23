cfd9519a040e33a5ca8526e257a04b82e8723b24
Meg Janickovic <42876672+megjanickovic@users.noreply.github.com>
2019-02-22 19:25:38 -0600

    Performing actions on other users
    Now you can add/remove/view/etc stickers from other users by mentioning them at the end of the command.

--- bot.js
+++ bot.js
@@ -32,10 +32,11 @@ helpMessage += `   !s remove - Takes away one of your stickers\n`;
 helpMessage += `   !s count - Displays the number of stickers you have\n`;
 helpMessage += `   !s view - View your lovely stickers!\n`;
 helpMessage += `   !s customize :emoji: - Changes the appearance of your stickers\n`;
-helpMessage += `   !s appearance - View your current sticker appearance`;
+helpMessage += `   !s appearance - View your current sticker appearance\n`;
+helpMessage += `   You can perform any of these commands on another user by mentioning them at the end\n`;
 
 client.on("ready", () => {
-	Stickers.sync({ force: true });
+	// Stickers.sync({ force: true });
 });
 
 client.on('message', async message => {
@@ -46,12 +47,19 @@ client.on('message', async message => {
 		let primaryCommand = splitCommand[0]; // The first word directly after the exclamation is the command
 		let args = splitCommand.slice(1); // All other words are arguments/parameters/options for the command
 		let user = message.author.username; 
+		
+		// Determine if the command should be applied to the person who called it,
+		// or a user passed as an argument
+		if (message.mentions.members.first()) {
+			user = message.mentions.members.first().user.username;
+		}
+		
 		let record = await Stickers.findOne({ where: { username: user } });
 		
 		if (!record) {
 			try {
 				record = await Stickers.create({
-					username: message.author.username,
+					username: user,
 				});
 			} catch (e) {
 				message.reply(`Error!`);
@@ -62,50 +70,50 @@ client.on('message', async message => {
 		switch (primaryCommand) {
 			case "add":
 				record.increment('stickers');
-				message.reply(`you earned a sticker!`);
+				message.channel.send(`${user} has earned a sticker!`);
 				break;
 			case "remove":
 				if (record.stickers > 0) {
 					record.decrement('stickers');
-					message.reply(`you lost a sticker!`);
+					message.channel.send(`${user} has lost a sticker!`);
 				} else {
-					message.reply(`you don't have any stickers to remove!`);
+					message.channel.send(`${user} doesn't have any stickers to remove!`);
 				}
 				break;
 			case "count":
 				if (record.stickers == 0) {
-					message.reply(`you don't have any stickers!`);
+					message.channel.send(`${user} doesn't have any stickers!`);
 				} else if (record.stickers == 1) {
-					message.reply(`you have one sticker!`);
+					message.channel.send(`${user} has one sticker!`);
 				} else {
-					message.reply(`you have ${record.stickers} stickers!`);
+					message.channel.send(`${user} has ${record.stickers} stickers!`);
 				}
 				break;
 			case "customize":
 				var sticker = args[0];
 				if (!sticker) {
-					message.reply(`invalid entry. Please try again.`);
+					message.channel.send(`Invalid entry. Please try again.`);
 					return;
 				}
 				const affectedRows = await Stickers.update({ sticker_appearance: args[0] }, { where: { username: user } });
 				if (affectedRows > 0) {
-					message.reply(`your stickers have been changed to ${args[0]}`);
+					message.channel.send(`${user}'sstickers have been changed to ${args[0]}`);
 				} else {
-					message.reply(`Error! Sticker appearance was not changed.`);
+					message.channel.send(`Error! Sticker appearance was not changed.`);
 				}
 				break;
 			case "appearance":
-				message.reply(`your stickers look like this -> ${record.sticker_appearance}`);
+				message.channel.send(`${user}'s stickers look like this -> ${record.sticker_appearance}`);
 				break;
 			case "view":
 				if (record.stickers == 0) {
-					message.reply(`you have no stickers!`);
+					message.channel.send(`${user} has no stickers!`);
 				} else {
 					var expanded_stickers = "";
 					for (var i = 0; i < record.stickers; i++) {
 						expanded_stickers += record.sticker_appearance;
 					}
-					message.reply(`Here are your stickers!\n${expanded_stickers}`);
+					message.channel.send(`Viewing ${user}'s sticker chart!\n${expanded_stickers}`);
 				}
 				break;
 			case "help":
